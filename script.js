760e6c8cd26d126ac63581c90545641815c2270f
ptadas22-lab <ptadas22@gmail.com>
2026-05-10 18:47:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ Also suggest best idea.
 
   try {
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyD8V0P_eGEohedfSlfTnntdt5DLTGi82ng",
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyD8V0P_eGEohedfSlfTnntdt5DLTGi82ng",
       {
         method: "POST",
         headers: {
